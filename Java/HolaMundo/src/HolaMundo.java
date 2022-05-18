abc8ec5e29a60417313ea10a04b865cfcef5bf42
José Carmona <pp_car88@hotmail.com>
2022-05-18 15:56:19 -0300

    Caracteres Especiales

--- Java/HolaMundo/src/HolaMundo.java
+++ Java/HolaMundo/src/HolaMundo.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde Java");
@@ -16,5 +19,21 @@ public class HolaMundo {
         System.out.println(miVariableCadena);
       
 // String miVariable= new String (""); ESTO ASÍ NO SE HACE
+
+String user = "Osvaldo";
+String titulo = "Ingeniero ";
+String union = user + " " +titulo;
+        System.out.println("union : " +union);
+
+short a, b;
+a=4;
+b=5;
+        System.out.println(a + b);
+//Si el 1er dato es de tipo cadena, entonces todo va ha ser uan cadena
+        
+//Clase Scanner
+Scanner entrada = new Scanner (System.in);
+String usuario ="Carlos"; 
+        System.out.println("usuario = "+usuario);
     }
 }
