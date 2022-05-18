a6cde37723ee97825a065db91237d2ab5ca3e0b3
José Carmona <pp_car88@hotmail.com>
2022-05-18 13:52:18 -0300

    Tipos de Variables

---
+++ Python/Lección 1/Ejercicio1.py
@@ -0,0 +1,19 @@
+'''
+Califica tu día. ¿Cómo estuvo tu día (1 al 10)?
+
+Mi día estuvo de: 10
+'''
+
+pregunta= int(input("¿Cómo estuvo tu día hoy? Califica tu día (1 al 10) "))
+print("Mi dia estuvo de: ",pregunta)
+
+'''
+Se solicita incluir la siguiente información acerca de un libro:
+titulo, autor
+'''
+
+título= input("Proporciona el título del libro: ")
+print(título)
+autor= input("Proporciona el autor del libro: ")
+print(autor)
+print(título,"fue escrito por",autor)
